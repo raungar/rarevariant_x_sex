afdef1027addc697f70a712e1616db4e3bd85115
R.A. Ungar <rachelungar@gmail.com>
2020-05-04 10:39:35 -0700

    add parameter sex_option, change to list.dirs
    1. added parameter sex_option which must be f/m/b or female/male/both
    2. changed list.files to list.dirs

--- Scripts/get_residuals_peer_by_sex.R
+++ Scripts/get_residuals_peer_by_sex.R
@@ -3,7 +3,8 @@ library("optparse") #for passing args
 
 get_opt_parser<-function(){
 	option_list = list(
-		 make_option(c("-d", "--dir_peer"), type="character", default=NULL)
+		 make_option(c("-d", "--dir_peer"), type="character", default=NULL),
+		 make_option(c("-s", "--sex_option"), type="character", default=NULL)
 		 ) 
 	
 	opt_parser = OptionParser(option_list=option_list)
@@ -15,16 +16,22 @@ get_opt_parser<-function(){
 
 opt_parser<-get_opt_parser()
 args<-parse_args(opt_parser)
-peer_dir<-args[1]
+peer_dir<-as.character(args[1])
+sex_option<-tolower(as.character(args[2]))
 
+print(sex_option)
 #check for file being passed, if not quit
-if(is.logical(peer_dir$help)){stop("ERROR: peer file dir not provided")}
+if(peer_dir == "FALSE"){stop("ERROR: peer file dir not provided")}
+if(sex_option != "m" & sex_option != "f" & sex_option != "b" & sex_option != "male" & sex_option != "female" & sex_option != "both" ){
+	stop("ERROR: sex_option must be the following: m, male, f, female, b, both")
+}
 
 print(peer_dir)
-print("FIN")
 
-
-for(file in list.files("RAREDIR/preprocessing_v8/PEER_v8/")){
-	print(file)
+for(this_dir in list.dirs(path=as.character(peer_dir))){
+	print(this_dir)
+	print("DIR ^")
 	break
 }
+
+print("EOF")
