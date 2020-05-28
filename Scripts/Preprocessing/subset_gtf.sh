86e746d08f9b694242d19f1df828d14b5ee4f9d2
raungar <raungar@stanford.edu>
2020-05-27 20:48:13 -0700

    subsets gtf into only lincRNA+protein-coding for x and autosome
    	-output: two gtf files, one for x, one for autosomes

--- Scripts/Preprocessing/subset_gtf.sh
+++ Scripts/Preprocessing/subset_gtf.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-gtf=$1 #input: general gencode gtf file
-out_a=$2 #output: autosomal only file
-out_x=$3 #output: x only file
+gtf=$1 #GTF FILE
+out_a=$2 #Out file: autosomal
+out_x=$3 #out file: x
+#gtf=/oak/stanford/groups/smontgom/shared/GTEx/all_data/GTEx_Analysis_2017-06-05_v8/references/gencode.v26.GRCh38.genes.gtf
 
-
-#take only autosomal genes
-#take only lincRNA and protein coding genes
-#outputs to out_a
 less -S $gtf | awk -F"[\t;]" '{if($1~/chr[0-9]{1,2}/){print $10"\t"$11}}' | sed 's/\"//g' | awk -F" " '{if($4=="protein_coding" || $4=="lincRNA"){print $2"\t"$4}}' > ${out_a}
-
-#take only x chromsome genes
-#take only lincRNA and protein coding genes
-#outputs to out_x
 less -S $gtf | awk -F"[\t;]" '{if($1=="chrX"){print $10"\t"$11}}' | sed 's/\"//g' | awk -F" " '{if($4=="protein_coding" || $4=="lincRNA"){print $2"\t"$4}}' > ${out_x}
