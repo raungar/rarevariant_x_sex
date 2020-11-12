2813a3ddd987d79d156bc9e9cb92301ff70c0e2c
raungar <raungar@stanford.edu>
2020-11-12 09:57:28 -0800

    update collapse_variants_gene.py and snakefile for new columns from linc/prot only

--- Scripts/Features/collapse_variants_genes.py
+++ Scripts/Features/collapse_variants_genes.py
@@ -7,31 +7,42 @@
 ############gene/ count of MAF<0.01 for ind /  MAF GTEX/ MAF GNOMAD nonFIN BOTH / 
 ############MAF GNOMAD nonFIN F/ MAF GNOMAD nonFIN M / minChr / Min Pos /
 ############ minRef / MinALT / minRefALT / min percent differ / geneTYPE
-
-
 import argparse
 import glob
 import re
 import gzip
 
-#parser = argparse.ArgumentParser(description='argparser')
-#parser.add_argument('dir_read', type=str, help='directory to read file from')
-#parser.add_argument('cutoff_mafdiff', type=float, help='cutoff [0-1] for % difference')
-#parser.add_argument('out_mafdiff', type=str, help='Output file for MAF Mvs.F diff')
-#parser.add_argument('out_m', type=str, help='outfile collaped m')
-#parser.add_argument('out_f', type=str, help='outfile collaped f')
-#parser.add_argument('out_b', type=str, help='outfile collaped both')
-#parser.add_argument('sex_file', type=str, help='GTEX sample file that can get sex')
-#args = parser.parse_args()
+parser = argparse.ArgumentParser(description='argparser')
+parser.add_argument('--dir_read', type=str, help='directory to read file from')
+parser.add_argument('--cutoff_mafdiff', type=str, help='cutoff [0-1] for % difference')
+parser.add_argument('--maf_filter',default=0.01, type=str, help='cutoff [0-1] for MAF of what is rare ')
+parser.add_argument('--out_mafdiff', type=str, help='Output file for MAF Mvs.F diff')
+parser.add_argument('--out_m', type=str, help='outfile collaped m')
+parser.add_argument('--out_f', type=str, help='outfile collaped f')
+parser.add_argument('--out_b', type=str, help='outfile collaped both')
+parser.add_argument('--sex_file', type=str, help='GTEX sample file that can get sex')
+args = parser.parse_args()
 
-dir_read="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/bySiteAnnoX/GenesAnno"
-out_mafdiff="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/maf_diff_x.tsv.gz"
-out_maf_both="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_both_x.tsv.gz"
-out_maf_m="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_m_x.tsv.gz"
-out_maf_f="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_f_x.tsv.gz"
-sex_file="/oak/stanford/groups/smontgom/shared/GTEx/all_data/GTEx_Analysis_2017-06-05_v8/sample_annotations/GTEx_Analysis_2017-06-05_v8_Annotations_SubjectPhenotypesDS_v2_downloaded_april2020.txt"
+dir_read=args.dir_read
+cutoff_mafdiff=float(args.cutoff_mafdiff)
+min_maf=float(args.maf_filter)
+out_mafdiff=args.out_mafdiff
+out_maf_both=args.out_b
+out_maf_m=args.out_m
+out_maf_f=args.out_f
+sex_file=args.sex_file
 
-min_maf=0.01 #### maf to filter on 
+print("arguments parsed.")
+#dir_read="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/bySiteAnnoX/GenesAnno"
+#out_mafdiff="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/maf_diff_x.tsv.gz"
+#out_maf_both="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_both_x.tsv.gz"
+#out_maf_m="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_m_x.tsv.gz"
+#out_maf_f="/oak/stanford/groups/smontgom/raungar/Sex/Output/features_v8/collapsed_maf_f_x.tsv.gz"
+#sex_file="/oak/stanford/groups/smontgom/shared/GTEx/all_data/GTEx_Analysis_2017-06-05_v8/sample_annotations/GTEx_Analysis_2017-06-05_v8_Annotations_SubjectPhenotypesDS_v2_downloaded_april2020.txt"
+#min_maf=0.01 #### maf to filter on 
+
+
+
 #compile at the beginning for speed
 af_both=re.compile("AF_nfe=")
 af_m=re.compile("AF_nfe_male=")
@@ -69,11 +80,12 @@ outwrite_maf_f.write(('\t'.join(map(str,colname_collapsed))+"\n").encode())
 for vartype in ["SNP","indel","SV"]:
 	## loop through all inds in this vartype
 	for f in glob.glob(dir_read+"/*"+vartype+"*"):
+		print(f)
 		#open file and read line by line
 		#get individual ID and sex of this individual
 		f_split=f.split("/")[-1]
 		f_split_again=f_split.split("_")
-		ind=f_split_again[1]
+		ind=next(filter(lambda x: re.search('GTEX',x),f_split_again))
 		sex=sex_key[ind]
 		## choose proper file to write to for sex specific
 		if sex == "male":
@@ -100,9 +112,14 @@ for vartype in ["SNP","indel","SV"]:
 				gtex_maf=line_split[3] #GTEx MAF
 				ref=line_split[5] #reference allele
 				alt=line_split[6] #alternate allele
-				ensg=(line_split[42]).split('\"')[1] # ENSG ID
-				genetype=((line_split[46]).split('\"'))[1] #lincRNA, proteincoding, etc
+				if(line_split[42] == "NA"):
+					next
+				ensg=(line_split[42]) #.split('\"')[1] # ENSG ID
+				#genetype=((line_split[54]).split('\"'))[1] #lincRNA, proteincoding, etc
+				genetype=((line_split[46])) #.split('\"'))[1] #lincRNA, proteincoding, etc
+				#varswitch=line_split[61].strip() ## A->C as AC
 				varswitch=line_split[53].strip() ## A->C as AC
+				#gnomad_anno=line_split[42] # GNOMAD annotation to further split
 				gnomad_anno=line_split[34] # GNOMAD annotation to further split
 				gnomad_split=gnomad_anno.split(';')
 				if gnomad_split[0] == "NO_MATCH":
@@ -113,14 +130,13 @@ for vartype in ["SNP","indel","SV"]:
 					gnomad_maf_both=float((([col for col in gnomad_split if af_both.match(col)])[0].split("="))[1])
 					gnomad_maf_m=float((([col for col in gnomad_split if af_m.match(col)])[0].split("="))[1])
 					gnomad_maf_f=float((([col for col in gnomad_split if af_f.match(col)])[0].split("="))[1])
-	
 				#get difference between MAF m vs F (controlling for divide by zero situation)
 				if (gnomad_maf_m+gnomad_maf_f)/2 == 0:
 					gnomad_maf_diff=0
 				else:
 					gnomad_maf_diff=(gnomad_maf_m-gnomad_maf_f)/((gnomad_maf_m+gnomad_maf_f)/2)
 				#only write to file those with a sex MAF differences of 10%
-				if gnomad_maf_diff > 0.1:
+				if gnomad_maf_diff > cutoff_mafdiff:
 					outwrite_mafdiff.write(("\t".join([chr,pos,ensg, vartype,ind,sex,ref,alt,varswitch,
 						gtex_maf, str(gnomad_maf_both),str(gnomad_maf_m),str(gnomad_maf_f),
 						genetype,str(gnomad_maf_diff)])+"\n").encode())
@@ -172,14 +188,13 @@ for vartype in ["SNP","indel","SV"]:
 					else:
 						store_line.append(1)
 						dic_sex[ensg]=store_line
-
 			##writes the dictionary to a file
 			for key_both in dic_both:			
-				outwrite_maf_both.write((key_both+"\t"+'\t'.join(map(str,dic_both[key_both]))+'\n').encode())
+				#outwrite_maf_both.write((key_both+"\t"+'\t'.join(map(str,dic_both[key_both]))+'\n').encode())
+				outwrite_maf_both.write(('\t'.join(map(str,dic_both[key_both]))+'\n').encode())
 			for key_both in dic_sex:			
-				outwrite_sex.write((key_both+"\t"+'\t'.join(map(str,dic_sex[key_both]))+'\n').encode())
-		break
-	break				
+				#outwrite_sex.write((key_both+"\t"+'\t'.join(map(str,dic_sex[key_both]))+'\n').encode())
+				outwrite_sex.write(('\t'.join(map(str,dic_sex[key_both]))+'\n').encode())
 
 outwrite_mafdiff.close()
 outwrite_maf_both.close()
