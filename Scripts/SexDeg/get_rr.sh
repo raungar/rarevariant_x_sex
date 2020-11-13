812fc3cbc6d69259d41c6ca4bf5ecb54cb9bb984
raungar <raungar@stanford.edu>
2020-11-13 13:03:07 -0800

    add rscript epitab, and reformat shell output for proper reading for rscript

--- Scripts/SexDeg/get_rr.sh
+++ Scripts/SexDeg/get_rr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-infile=$1
-outfile=$2
+#infile=$1
+#outfile=$2
 
 #infile="Output/sexdeg_v8/all_genes_sexDEGs_rvs_BREAST.txt.gz"
+infile="Output/sexdeg_v8/all_genes_sexDEGs_rvs_BREAST.txt.gz"
 
 num_sexdegs=0
 num_nonsexdegs=0
@@ -15,13 +16,13 @@ vals=`less $infile | awk -F"\t" -v num_sexdegs=$num_sexdegs -v num_nonsexdegs=$n
 	if ($2 !="beta"){
 		if ($2 == 0){
 			num_nonsexdegs=num_nonsexdegs+1
-			if ($4 == 1){
+			if ($4 != 1){
 				num_rvs_nonsexdegs=num_rvs_nonsexdegs+1
 
 			}
 		} else {
 			num_sexdegs=num_sexdegs+1
-			if ($4 == 1){
+			if ($4 != 1){
 				num_rvs_sexdegs=num_rvs_sexdegs+1
 			}
 		}
@@ -29,8 +30,8 @@ vals=`less $infile | awk -F"\t" -v num_sexdegs=$num_sexdegs -v num_nonsexdegs=$n
 	}
 }' | tail -1`
 
-echo "num_rvs_nonsexdegs,num_nonsexdegs,num_rvs_sexdegs,num_sexdegs" > $outfile
-echo "$vals">$outfile
+echo "num_rvs_nonsexdegs num_nonsexdegs num_rvs_sexdegs num_sexdegs" #> $outfile
+echo "$vals" #>$outfile
 #echo "$num_rvs_sexdegs over $num_sexdegs"
 #echo "--------------------------------------"
 #echo "$num_rvs_nonsexdegs over $num_nonsexdegs"
