98f67b83f36f3e2f67b74a4e1e5b7016e87a28b7
raungar <raungar@stanford.edu>
2020-11-16 14:14:35 -0800

    update for addition of chr col

--- Scripts/SexDeg/get_rr.sh
+++ Scripts/SexDeg/get_rr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#infile=$1
-#outfile=$2
+infile=$1
+outfile=$2
 
 #infile="Output/sexdeg_v8/all_genes_sexDEGs_rvs_BREAST.txt.gz"
-infile="Output/sexdeg_v8/all_genes_sexDEGs_rvs_BREAST.txt.gz"
+#infile="Output/sexdeg_v8/all_genes_sexDEGs_rvs_BREAST.txt.gz"
 
 num_sexdegs=0
 num_nonsexdegs=0
@@ -13,16 +13,16 @@ num_rvs_sexdegs=0
 num_rvs_nonsexdegs=0
 vals=`less $infile | awk -F"\t" -v num_sexdegs=$num_sexdegs -v num_nonsexdegs=$num_nonsexdegs -v num_rvs_sexdegs=$num_rvs_sexdegs -v num_rvs_nonsexdegs=$num_rvs_nonsexdegs '{
 	#if beta is not zero means that it is NOT a sexDEG, skip header
-	if ($2 !="beta"){
-		if ($2 == 0){
+	if ($3 !="beta"){
+		if ($3 == 0){
 			num_nonsexdegs=num_nonsexdegs+1
-			if ($4 != 1){
+			if ($5 != 1){
 				num_rvs_nonsexdegs=num_rvs_nonsexdegs+1
 
 			}
 		} else {
 			num_sexdegs=num_sexdegs+1
-			if ($4 != 1){
+			if ($5 != 1){
 				num_rvs_sexdegs=num_rvs_sexdegs+1
 			}
 		}
@@ -30,8 +30,8 @@ vals=`less $infile | awk -F"\t" -v num_sexdegs=$num_sexdegs -v num_nonsexdegs=$n
 	}
 }' | tail -1`
 
-echo "num_rvs_nonsexdegs num_nonsexdegs num_rvs_sexdegs num_sexdegs" #> $outfile
-echo "$vals" #>$outfile
+echo "num_rvs_nonsexdegs num_nonsexdegs num_rvs_sexdegs num_sexdegs" > $outfile
+echo "$vals" >> $outfile
 #echo "$num_rvs_sexdegs over $num_sexdegs"
 #echo "--------------------------------------"
 #echo "$num_rvs_nonsexdegs over $num_nonsexdegs"
