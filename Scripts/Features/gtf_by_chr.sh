46abd25b274917effaf0e1a6e7fd93df9c07ee81
raungar <raungar@stanford.edu>
2020-11-17 11:09:38 -0800

    update for prot linc restriction

---
+++ Scripts/Features/gtf_by_chr.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+gtf=$1
+out_prefix=$2
+
+echo "gtf input is: $gtf"
+echo "out_prefix input is: $out_prefix"
+
+
+#check if files exist
+if ls $out_prefix/*gtf 1> /dev/null 2>&1
+then
+	rm $out_prefix/*gtf
+fi
+echo "any existing gtf files removed."
+
+
+while read line
+do
+	echo "reading: $line"
+	type=`echo $line | awk '{print $3}'`	
+	echo $type
+	if [[ $type != "gene" ]]
+	then
+		continue
+	fi
+	chr_num=`echo $line | awk '{print $1}'`
+	echo "$out_prefix/$chr_num.presort.gtf"
+	echo $line | sed 's/\s/\t/g' | awk -F"\t" '{for(i=1; i<=20; i++){printf $i"\t"} ;print""}'  >> $out_prefix/$chr_num.presort.gtf
+
+done < $gtf
+
+####this is necessary to sort after alll is split
+echo "sort gtf files"
+for file in `ls $out_prefix/*presort.gtf`
+do
+	file_chrnum=`echo $file | awk -F"/" '{print $NF}' | awk -F"." '{print $1}'`
+	echo $file_chrnum
+	cat $file | awk -F "\t" '{OFS="\t"}{printf $1"\t"$4"\t"$5; for(i=6; i<=20;i++){printf "\t"$i} print ""}'  | sort -k 1,1 -k2,2n >  $out_prefix/$file_chrnum.gtf
+	rm $file
+done
+
